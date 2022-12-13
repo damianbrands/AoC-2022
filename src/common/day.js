c5b1933ac28d17ebaf6ad7796e9c105bdcb6fac6
Bino <74612564+BinoGH@users.noreply.github.com>
2022-12-13 21:09:16 +0100

    Day2 complete

--- src/common/day.js
+++ src/common/day.js
@@ -54,6 +54,66 @@ const Day = props => {
         return (elf1 + elf2 + elf3);
     }
 
+    function day2(){
+        const moves = puzzleInput.split(" ");
+        const elfMoves = [];
+        const yourMoves = [];
+        let totalScore = 0;
+
+        for(let i = 0; i < moves.length; i++){
+            (i % 2 === 0 ? elfMoves : yourMoves).push(moves[i]);
+        }
+
+        for(let i = 0; i < elfMoves.length; i++){
+            let rpsYou = 0;
+
+            switch(yourMoves[i]){
+                case 'X':
+                    rpsYou += 1;
+                    break;
+                case 'Y':
+                    rpsYou += 2;
+                    break;
+                case 'Z':
+                    rpsYou += 3;
+                    break;
+            }
+            totalScore += rpsYou;
+            totalScore += rpsScore(elfMoves[i], rpsYou);
+        }
+
+        return totalScore;
+    }
+
+    function rpsScore(elfMove, rpsYou){
+        let rpsElf = 0;
+        switch (elfMove){
+            case 'A':
+                rpsElf = 1;
+                break;
+            case 'B':
+                rpsElf = 2;
+                break;
+            case 'C':
+                rpsElf = 3;
+                break;
+            default:
+                return null;
+        }
+        if(rpsElf === rpsYou){
+            return 3;
+        }
+        if((rpsElf === 1 && rpsYou === 3) || (rpsElf === 2 && rpsYou === 1) || (rpsElf === 3 && rpsYou === 2)){
+            return 0;
+        }
+        return 6;
+    }
+
+    function day2Star(){
+
+        return 0;
+    }
+
     return (
         <>
             <div className={"DayNumber"}>
@@ -66,7 +126,8 @@ const Day = props => {
                         case 1: setAnswerNormal(day1());
                                 setAnswerStar(day1Star());
                                 break;
-                        case 2:
+                        case 2: setAnswerNormal(day2());
+                                setAnswerStar(day2Star());
                                 break;
                         default: setAnswerNormal(0);
                     }
